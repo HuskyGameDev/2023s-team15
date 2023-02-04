fbe3d774956a5ffc29b343ade62be04a515c0c7e
SamDuFort <samuelrobertdufort@gmail.com>
2023-02-03 20:49:15 -0500

    Added Floor, Wall, Jump, and Gravity

--- super_six_shooter/Assets/Scripts/PlayerScripts/playerController.cs
+++ super_six_shooter/Assets/Scripts/PlayerScripts/playerController.cs
@@ -6,6 +6,12 @@ public class playerController : MonoBehaviour
 {
     private Rigidbody2D m_Rigidbody2D;
     private Vector3 m_Velocity = Vector3.zero;
+    private Vector3 m_jumpVelocity = Vector3.zero;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
 
     private void Awake() 
     {
@@ -22,7 +28,21 @@ public class playerController : MonoBehaviour
 
         //Implementation of "flip sprite" would go here
 
-        //Jump capabilities could potentially go here
+    }
+
+    //This is a method specifically for jumping
+    public void Jump(float move)
+    {
+	isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+	if(isTouchingGround)
+	{
+		//Vector3 targetJumpVelocity = new Vector2(m_Rigidbody2D.velocity.x, move * 10f);
+		//I tried using this smooth thing, and it made the jumping really floaty.
+		//Since I am not using the below line, then I don't need the above line. However, I have left them here just in case I or someone else might need to use them.
+		//m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetJumpVelocity, ref m_jumpVelocity, 0f);
+		m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, move * 10f);//Keeps the smae x velocity, and adds to the y velocity.
+	}
+
     }
 
     // Update is called once per frame
--- super_six_shooter/Assets/Scripts/PlayerScripts/playerMove.cs
+++ super_six_shooter/Assets/Scripts/PlayerScripts/playerMove.cs
@@ -8,18 +8,22 @@ public class playerMove : MonoBehaviour
     public playerController controller;
 
     [SerializeField] public float runSpeed = 40f;
+    [SerializeField] public float jumpSpeed = 50f;
 
     float horizontalMove = 0f;
+    float verticalMove = 0f;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //Assign direction of movement * speed
+	verticalMove = Input.GetAxisRaw("Jump") * jumpSpeed; //Assign direction of movement * speed
     }
 
     void FixedUpdate()
     {
         // Move our character
         controller.Move(horizontalMove * Time.fixedDeltaTime);
+	controller.Jump(verticalMove * Time.fixedDeltaTime);
     }
 }
