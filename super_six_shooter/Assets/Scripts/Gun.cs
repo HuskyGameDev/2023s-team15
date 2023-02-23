21cc4d3372441082e1100dc96189544a9393171b
cmroth <90865077+cmroth@users.noreply.github.com>
2023-02-23 08:43:03 -0500

    Mouse Firing
    Bullet will follow direction of the mouse upon firing with a right click. Looks a bit goofy but it works.

--- super_six_shooter/Assets/Scripts/Gun.cs
+++ super_six_shooter/Assets/Scripts/Gun.cs
@@ -7,17 +7,40 @@ public class Gun : MonoBehaviour
     public Transform firePoint;
     public GameObject fbulletPrefab;
 
+    public Transform MousePoint;
+
     // Update is called once per frame
     void Update()
     {
+        mouseDirection();
+
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            mouseShoot();
         }
     }
 
     void Shoot()
     {
         Instantiate(fbulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    void mouseShoot()
+    {
+        Instantiate(fbulletPrefab, MousePoint.position, MousePoint.rotation);
+    }
+
+    void mouseDirection()
+    {
+        Vector3 input = Input.mousePosition;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(input);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        MousePoint.transform.right = direction;
     }
 }
